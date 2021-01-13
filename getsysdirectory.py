4f01f5b4a64346e053be9c037781bfdfe6e44e2d
Ronald Hoang <ronald.hoang@lordstownmotors.com>
2021-01-13 12:24:21 -0500

    Fixed some issues

---
+++ getsysdirectory.py
@@ -0,0 +1,3 @@
+def getroute():
+	dataroute = os.path.join(os.path.expanduser("~"),"Lordstown Motors Corp", "Battery & Propulsion Team - General","Documents", "materials.db")
+	return dataroute
--- setup.py
+++ setup.py
@@ -12,14 +12,18 @@ multiprocessing.set_start_method('spawn')
 
 from multiprocessing.managers import BaseManager
 
+entry_point = sys.argv[1]
+sys.argv.pop()
 sys.argv.append('py2exe')
+sys.argv.append('q')
 
-setup(
-options = { 'py2exe': {'bundle_files': 2, 'compressed': True}},
+setup( console = [entry_point],
+options = { 'py2exe': {'bundle_files': 1, 'compressed': True, 'optimize': 2}},
 	windows=[{
 		'script':"SQLiteGUI.py",
 		'icon_resources': [(1, 'Lordstown.ICO')]
 	 }],
 	zipfile = None,
-	name = "LMC B&P MaterialsDatabase"
+	name = "LMCDatabase"
 )
+
